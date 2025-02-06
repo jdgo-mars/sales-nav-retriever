b80193552cf5809368e36f699e29ebd8b298dad9
João Martins <5918114+jdgo-mars@users.noreply.github.com>
2025-02-06 21:17:55 +0000

    commit

--- wxt.config.ts
+++ wxt.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'wxt';
+import { defineConfig } from "wxt";
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
-  extensionApi: 'chrome',
-  modules: ['@wxt-dev/module-react'],
+  extensionApi: "chrome",
+  modules: ["@wxt-dev/module-react"],
+  manifest: {
+    permissions: ["activeTab", "scripting", "clipboardWrite"],
+    "web_accessible_resources": [
+    {
+      "matches": ["*://*.linkedin.com/*"],
+      "resources": ["icon/*.png"]
+    }
+  ]
+  },
+  
 });
